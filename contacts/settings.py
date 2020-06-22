b960361290f65733fe18dec971af9705119c70e8
sohel <sohelahmed4646@gmail.com>
2020-06-22 15:13:47 +0600

    settings file update

--- contacts/settings.py
+++ contacts/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_e$)%_8#st--a=hbpymdmo&g%(r$+7q&du)f_=*0cka@p-od0w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
